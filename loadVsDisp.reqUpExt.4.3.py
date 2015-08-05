370b4097016dc3d136f9bf8440065500310f4fab
Cameron Smith <smithc11@rpi.edu>
2015-08-05 09:35:55 -0400

    use plot selection over time to control time

--- loadVsDisp.reqUpExt.4.3.py
+++
@@ -1,7 +0,0 @@
-print '+++requestUpdateExtent'
-info = inputs[0].GetInformation()
-# Ask for the next timestep.
-print '~~~~timeIdx', self.timeIdx, 'self.timeSteps[self.timeIdx]', self.timeSteps[self.timeIdx]
-info.Set(vtk.vtkStreamingDemandDrivenPipeline.UPDATE_TIME_STEP(),
-   self.timeSteps[self.timeIdx])
-print '---requestUpdateExtent'
