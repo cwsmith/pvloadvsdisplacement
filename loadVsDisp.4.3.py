c93864e57de7410a6f15ac55cf6b5ca72d414638
Cameron Smith <smithc11@rpi.edu>
2015-08-06 00:53:11 -0400

    xml plugin with controls over solution specific variables
    python_filter_generator.py was used to generate the xml filter. see http://www.kitware.com/blog/home/post/534

--- loadVsDisp.4.3.py
+++ loadVsDisp.4.3.py
@@ -1,32 +1,42 @@
-import numpy as np
-import paraview.vtk.numpy_interface.algorithms as alg 
-from vtk import vtkFloatArray
+Name = 'Load Vs Displacement'
+Label = 'Load Vs Displacement'
+Help = 'Plot the maximum load vs the maximum displacement for a selected set of elements'
 
-def maxStress(inp):
-  base = 'Cauchy_Stress_'
-  max_stress = 0
-  for i in range(1,6):
-    name = base + str(i)
-    s = inp.CellData[name]
-    ms = alg.max(alg.eigenvalue(s))
-    if ms > max_stress:
-      max_stress = ms
-  return max_stress
+NumberOfInputs = 1
+InputDataType = 'vtkUnstructuredGrid'
+OutputDataType = 'vtkTable' # omit this line to use 'same as input'
+ExtraXml = ''
 
-def addColumn(table,name,v):
-  c = vtkFloatArray()
-  c.SetName(name)
-  c.InsertNextValue(v)
-  table.AddColumn(c)
+Properties = dict(
+  numStressFields = 0,
+  stressFieldName = 'Cauchy_Stress_',
+  displacementFieldName = 'Solution'
+)
 
-info = inputs[0].GetInformation()
-t=info.Get(vtk.vtkDataObject.DATA_TIME_STEP())
+def RequestData():
+  import numpy as np
+  import paraview.vtk.numpy_interface.algorithms as alg 
+  from vtk import vtkFloatArray
 
-max_stress = maxStress(inputs[0])
+  def maxStress(inp):
+    max_stress = 0
+    for i in range(1,numStressFields+1):
+      name = stressFieldName + str(i)
+      s = inp.CellData[name]
+      ms = alg.max(alg.eigenvalue(s))
+      if ms > max_stress:
+        max_stress = ms
+    return max_stress
+  
+  def addColumn(table,name,v):
+    c = vtkFloatArray()
+    c.SetName(name)
+    c.InsertNextValue(v)
+    table.AddColumn(c)
 
-d = inputs[0].PointData["Solution"]
-max_disp = alg.max_per_block(alg.mag(d)).Arrays[0]
-
-addColumn(output,'time',t)
-addColumn(output,'max_disp',max_disp)
-addColumn(output,'max_stress',max_stress)
+  max_stress = maxStress(inputs[0])
+  d = inputs[0].PointData[displacementFieldName]
+  max_disp = alg.max_per_block(alg.mag(d)).Arrays[0]
+  
+  addColumn(output,'max_disp',max_disp)
+  addColumn(output,'max_stress',max_stress)
